530d26a0e920430ae9113c49f5abfa956db7a42e
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-08-08 11:08:39 +0200

    time search can now handle multiple entries with the same time correctly

--- src/main/java/com/bc/inventory/utils/Search.java
+++ src/main/java/com/bc/inventory/utils/Search.java
@@ -1,19 +1,40 @@
 package com.bc.inventory.utils;
 
 /**
- * @author marcoz
+ * @author MarcoZ
  */
 public class Search {
 
-    public static int indexedBinarySearch(int[] startTimes, int currentStartTime) {
+    /**
+        * Searches the specified array of ints for the specified value using the
+        * binary search algorithm.  The array must be sorted (as
+        * by the {@link java.util.Arrays#sort(int[])} method) prior to making this call.  If it
+        * is not sorted, the results are undefined.  If the array contains
+        * multiple elements with the specified value, the first values is returned.
+        *
+        * @param array the array to be searched
+        * @param key the value to be searched for
+        * @return index of the search key, if it is contained in the array;
+        *         otherwise, <tt><i>insertion point</i> - 1</tt>.  The
+        *         <i>insertion point</i> is defined as the point at which the
+        *         key would be inserted into the array: the index of the first
+        *         element greater than the key, or <tt>a.length</tt> if all
+        *         elements in the array are less than the specified key.
+        */
+    public static int indexedBinarySearch(int[] array, int key) {
         int low = 0;
-        int high = startTimes.length - 1;
+        int high = array.length - 1;
         while (low <= high) {
             int mid = (low + high) >>> 1;
-            final int t1 = startTimes[mid];
-            if (t1 < currentStartTime) {
+            final int t1 = array[mid];
+            if (t1 < key) {
                 low = mid + 1;
-            } else if (t1 == currentStartTime) {
+            } else if (t1 == key) {
+                // if there multiple equal values next to each other, 
+                // make sure to return the index to first of them
+                while (mid > 0 && array[mid - 1] == key) {
+                    mid--;
+                }
                 return mid; // key found
             } else {
                 high = mid - 1;
--- src/test/java/com/bc/inventory/utils/SearchTest.java
+++ src/test/java/com/bc/inventory/utils/SearchTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class SearchTest {
     @Test
-    public void indexedBinarySearch() throws Exception {
+    public void search() throws Exception {
         int[] values = {3, 10, 20};
         assertEquals(0, Search.indexedBinarySearch(values, 1));
         assertEquals(0, Search.indexedBinarySearch(values, 2));
@@ -19,5 +19,41 @@ public class SearchTest {
         assertEquals(0, Search.indexedBinarySearch(values, 9));
         assertEquals(1, Search.indexedBinarySearch(values, 10));
         assertEquals(1, Search.indexedBinarySearch(values, 11));
+        assertEquals(2, Search.indexedBinarySearch(values, 20));
+        assertEquals(2, Search.indexedBinarySearch(values, 21));
     }
+    
+    @Test
+    public void oneValue() throws Exception {
+        int[] values = {2};
+        assertEquals(0, Search.indexedBinarySearch(values, 1));
+        assertEquals(0, Search.indexedBinarySearch(values, 2));
+        assertEquals(0, Search.indexedBinarySearch(values, 3));
+    }
+
+    @Test
+    public void equalValues() throws Exception {
+        int[] values = {3, 4, 4};
+        assertEquals(0, Search.indexedBinarySearch(values, 1));
+        assertEquals(0, Search.indexedBinarySearch(values, 2));
+        assertEquals(0, Search.indexedBinarySearch(values, 3));
+        assertEquals(1, Search.indexedBinarySearch(values, 4));
+        assertEquals(2, Search.indexedBinarySearch(values, 5));
+        
+        values = new int[]{4, 4, 4};
+        assertEquals(0, Search.indexedBinarySearch(values, 1));
+        assertEquals(0, Search.indexedBinarySearch(values, 2));
+        assertEquals(0, Search.indexedBinarySearch(values, 3));
+        assertEquals(0, Search.indexedBinarySearch(values, 4));
+        assertEquals(2, Search.indexedBinarySearch(values, 5));
+
+        values = new int[]{4, 4, 4, 4, 4};
+        assertEquals(0, Search.indexedBinarySearch(values, 1));
+        assertEquals(0, Search.indexedBinarySearch(values, 2));
+        assertEquals(0, Search.indexedBinarySearch(values, 3));
+        assertEquals(0, Search.indexedBinarySearch(values, 4));
+        assertEquals(4, Search.indexedBinarySearch(values, 5));
+        
+    }
+    
 }
